644b0b47972c6399e964ce646511624fb18d5297
bowen-wu <13793248162@163.com>
2017-12-31 10:10:15 +0800

    updata

--- js/main.js
+++ js/main.js
@@ -1,50 +1,72 @@
 window.onload = function () {
-    var obj = {
-        0: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-        1: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-        2: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-        3: ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
-        length: 4
+    //初始化
+    var init= init();
+    var obj= init.obj;
+    var hash = init.hash;
+    //生成键盘
+    generateKeyboard(obj)
+    //监听键盘事件
+    listenerKeyboardEvent(hash)
+    
+    //工具函数
+    function init(){
+        var obj = {
+            0: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+            1: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+            2: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+            3: ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+            length: 4
+        }
+        var hash = {
+            q: 'qq.com',
+            w: 'weibo.com',
+            e: 'ele.me',
+            r: 'renren.com',
+            t: 'taobao.com',
+            a: 'alibabagroup.com',
+            j: 'jd.com',
+            z: 'zhihu.com',
+        }
+        var userDefined = JSON.parse(localStorage.getItem('userKey') || 'null');
+        if (userDefined) {
+            hash = userDefined;
+        }
+        return {
+            obj: obj,
+            hash:hash
+        }
     }
-    var hash = {
-        q: 'qq.com',
-        w: 'weibo.com',
-        e: 'ele.me',
-        r: 'renren.com',
-        t: 'taobao.com',
-        a: 'alibabagroup.com',
-        j: 'jd.com',
-        z: 'zhihu.com',
-    }
-    var userDefined = JSON.parse(localStorage.getItem('userKey') || 'null');
-    if (userDefined) {
-        hash = userDefined;
-    }
-    var container = document.getElementById('container');
-    for (var i = 0, len = obj.length; i < len; i++) {
-        var div = createdNewElement('div', container);
-        for (var j = 0, leng = obj[i].length; j < leng; j++) {
-            var kbd = createdNewElement('kbd', div, obj[i][j]);
-            kbd.id = obj[i][j];
-            var btn = createdNewElement('button', kbd, 'Edit');
-            btn.onclick = function (event) {
-                changeUrl(event.target.parentNode.id);
+    function generateKeyboard(obj){
+        var container = document.getElementById('container');
+        for (var i = 0, len = obj.length; i < len; i++) {
+            var div = createdNewElement('div', container, 'row');
+            for (var j = 0, leng = obj[i].length; j < leng; j++) {
+                var kbd = createdNewElement('kbd', div, 'key');
+                kbd.id = obj[i][j];
+                var span = createdNewElement('span', kbd, 'letter', obj[i][j])
+                var btn = createdNewElement('button', kbd, 'btn', 'E');
+                btn.onclick = function (event) {
+                    changeUrl(event.target.parentNode.id);
+                }
             }
         }
     }
-    document.onkeypress = function (event) {
-        var key = event.key;
-        if (hash[key] != undefined) {
-            window.open('http://' + hash[key], '_blank');
-        } else if (key.replace(/^[A-Za-z0-9]+$/)) { //　正则　复制
-            changeUrl(key);
-        } else {
-            alert('请输入数字或者字母');
+    function listenerKeyboardEvent(hash) {
+        document.onkeypress = function (event) {
+            var key = event.key;
+            if (hash[key] != undefined) {
+                window.open('http://' + hash[key], '_blank');
+            } else if (key.replace(/^[A-Za-z0-9]+$/)) { //　正则　复制
+                changeUrl(key);
+            } else {
+                alert('请输入数字或者字母');
+            }
         }
     }
 
-    function createdNewElement(ele, parent, content) {
+    function createdNewElement(ele, parent, className,content) {
         var ele = document.createElement(ele);
+        ele.className= className;
         parent.appendChild(ele);
         ele.textContent = content;
         return ele;
