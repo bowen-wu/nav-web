0ef3aa062f7b40cd8268f2904fb14ca4f5aca2b3
bowen-wu <13793248162@163.com>
2018-01-24 14:38:00 +0800

    优化代码 + 修改 README.md 文件

--- js/once-keyboard.js
+++ js/once-keyboard.js
@@ -1,4 +1,4 @@
-!function(){
+!function () {
     let view = document.getElementById('container')
     let model = {
         obj: {
@@ -36,19 +36,19 @@
             n: '',
             m: 'developer.mozilla.org'
         },
-        init: function(){
+        init: function () {
             return {
                 keyboard: this.obj,
                 website: this.hash
             }
         },
-        fetch: function(){
+        fetch: function () {
             var userDefined = JSON.parse(localStorage.getItem('userKey'));
             if (userDefined) {
                 return userDefined
             }
         },
-        save: function(saveDate){
+        save: function (saveDate) {
             localStorage.setItem('userKey', JSON.stringify(saveDate))
         }
     }
@@ -69,7 +69,7 @@
         cancel: null,
         modifyKey: null,
         modifyEle: null,
-        init: function(){
+        init: function () {
             this.view = view
             this.model = model
             this.mask = this.view.querySelector('#mask')
@@ -89,135 +89,140 @@
             this.cancel = this.view.querySelector('.cancel')
             this.bindEvents()
         },
-        bindEvents: function(){
-            document.addEventListener('keyup',(event) => {
-                if (event.keyCode >= 65 && event.keyCode <= 90) {
-                    if (this.website[event.key]) {
-                        window.open('http://' + this.website[event.key], '_blank')
-                    } else {
-                        this.modifyKey = event.key
-                        this.modifyEle = this.view.querySelector('#' + event.key)
-                        this.view.querySelector('#pressupKeyboard').textContent = '[ ' + event.key + ' ]'
-                        this.mask.classList.add('active')
-                    }
-                }
-            })
-            this.letterKeyboard.forEach((item) => {
-                item.addEventListener('click',(event) => {
-                    if(this.website[event.currentTarget.id]){
-                        window.open('http://' + this.website[event.currentTarget.id],'_blank')
-                    }else{
-                        this.modifyKey = event.currentTarget.id
-                        this.modifyEle = event.currentTarget
-                        this.view.querySelector('#pressupKeyboard').textContent = '[ ' + event.currentTarget.id + ' ]'
-                        this.mask.classList.add('active')
-                    }
-                })
-            })
-            this.edit.forEach((item) => {
-                item.addEventListener('click', (event) => {
-                    event.stopPropagation()
-                    this.modifyKey = event.currentTarget.parentNode.id
-                    this.modifyEle = event.currentTarget.parentNode
-                    this.view.querySelector('#pressupKeyboard').textContent = '[ ' + event.currentTarget.parentNode.id + ' ]'
-                    this.mask.classList.add('active')
-                })
-            })
-
-            this.inputWebsite.addEventListener('keyup', (event) => {
-                event.stopPropagation()
-                this.inputWebsite.classList.remove('animated', 'shake')
-                this.reminder.classList.remove('active')
-                this.userInput = event.currentTarget.value
-                if (event.keyCode === 13) {
-                    if (this.userInput && this.userInput.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi)) {
-                        this.inputWebsite.value = ''
-                        this.mask.classList.remove('active')
-                        let saveDate = JSON.parse(localStorage.getItem('userKey')) || this.website
-                        saveDate[this.modifyKey] = this.userInput
-                        this.modifyEle.querySelector('img').src = 'http://' + this.userInput + '/favicon.ico'
-                        this, model.save(saveDate)
-                        window.open('http://' + this.userInput, '_blank')
-                        this.userInput = ''
-                    } else {
-                        this.inputWebsite.classList.add('animated', 'shake')
-                            this.inputWebsite.value = ''
-                            this.reminder.classList.add('active')
-                    }
-                }
-            })
-            this.confirm.addEventListener('click',() => {
-                if (this.userInput && this.userInput.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi)) {
-                    this.inputWebsite.value = ''
-                    this.mask.classList.remove('active')
-                    let saveDate = JSON.parse(localStorage.getItem('userKey')) || this.website
-                    saveDate[this.modifyKey] = this.userInput
-                    this.modifyEle.querySelector('img').src = 'http://' + this.userInput + '/favicon.ico'
-                    this,model.save(saveDate)
-                    window.open('http://' + this.userInput, '_blank')
-                    this.userInput = ''
-                } else {
-                    this.inputWebsite.classList.add('animated','shake')
-                    this.inputWebsite.value = ''
-                    this.reminder.classList.add('active')
-                }
-            })
-            this.cancel.addEventListener('click',(event) => {
-                this.mask.classList.remove('active')
-            })
-
-            this.webIcon.forEach((item) => {
-                item.addEventListener('error', (event) => {
-                    event.currentTarget.src = './image/smilingFace.png'
-                })
-            })
-        },
-        createKeyboard: function(){
+        createKeyboard: function () {
             for (let i = 0, len = this.keyboard.length; i < len; i++) {
                 let row = this.createEle({
                     ele: 'div',
                     klassName: 'row',
                     parent: this.view
                 })
-                for(let j = 0,len = this.keyboard[i].length; j < len; j++){
-                    if(typeof this.keyboard[i][j] === 'number'){
+                for (let j = 0, len = this.keyboard[i].length; j < len; j++) {
+                    if (typeof this.keyboard[i][j] === 'number') {
                         this.row = row
-                        this.createNumberKeyboard({textContent: this.keyboard[i][j]})
-                    }else if(this.keyboard[i][j]){
+                        this.createNumberKeyboard({ textContent: this.keyboard[i][j] })
+                    } else if (this.keyboard[i][j]) {
                         this.row = row
-                        this.createLetterKeyboard({contentAndId: this.keyboard[i][j]})
-                    }else{
+                        this.createLetterKeyboard({ contentAndId: this.keyboard[i][j] })
+                    } else {
                         this.row = row
                         this.createShiftKeyboard()
                     }
                 }
             }
             this.view.lastChild.lastChild.lastChild.textContent = 'Shift'
+        },
+        bindEvents: function () {
+            document.addEventListener('keyup', (event) => {
+                if (event.keyCode >= 65 && event.keyCode <= 90) {
+                    if (this.website[event.key]) {
+                        this.gotoWebsite({
+                            website: this.website[event.key]
+                        })
+                    } else {
+                        this.popupDialog({
+                            modifyKey: event.key,
+                            modifyEle: this.view.querySelector('#' + event.key)
+                        })
+                    }
+                }
+            })
+            this.letterKeyboard.forEach((item) => {
+                item.addEventListener('click', (event) => {
+                    if (this.website[event.currentTarget.id]) {
+                        this.gotoWebsite({
+                            website: this.website[event.currentTarget.id]
+                        })
+                    } else {
+                        this.popupDialog({
+                            modifyKey: event.currentTarget.id,
+                            modifyEle: event.currentTarget
+                        })
+                    }
+                })
+            })
+            this.edit.forEach((item) => {
+                item.addEventListener('click', (event) => {
+                    event.stopPropagation()
+                    this.popupDialog({
+                        modifyKey: event.currentTarget.parentNode.id,
+                        modifyEle: event.currentTarget.parentNode
+                    })
+                })
+            })
 
+            this.inputWebsite.addEventListener('keyup', (event) => {
+                event.stopPropagation()
+                this.resetDialog({ userInput: event.currentTarget.value })
+                if (event.keyCode === 13) {
+                    this.gotoUserInputWebsiteAndSave()
+                }
+            })
+            this.confirm.addEventListener('click', () => {
+                this.gotoUserInputWebsiteAndSave()
+            })
+            this.cancel.addEventListener('click', (event) => {
+                this.resetDialog({ userInput: '' })
+                this.mask.classList.remove('active')
+            })
+            this.webIcon.forEach((item) => {
+                item.addEventListener('error', (event) => {
+                    event.currentTarget.src = './image/smilingFace.png'
+                })
+            })
+        },
+        resetDialog: function ({ userInput }) {
+            this.inputWebsite.classList.remove('animated', 'shake')
+            this.reminder.classList.remove('active')
+            this.userInput = userInput
+        },
+        gotoWebsite: function ({ website }) {
+            window.open('http://' + website, '_blank')
+        },
+        popupDialog: function ({ modifyKey, modifyEle }) {
+            this.modifyKey = modifyKey
+            this.modifyEle = modifyEle
+            this.view.querySelector('#pressupKeyboard').textContent = '[ ' + modifyKey + ' ]'
+            this.mask.classList.add('active')
+        },
+        gotoUserInputWebsiteAndSave: function () {
+            this.inputWebsite.value = ''
+            if (this.userInput && this.userInput.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi)) {
+                this.mask.classList.remove('active')
+                this.website[this.modifyKey] = this.userInput
+                let saveDate = JSON.parse(localStorage.getItem('userKey')) || this.website
+                saveDate[this.modifyKey] = this.userInput
+                this.modifyEle.querySelector('img').src = 'http://' + this.userInput + '/favicon.ico'
+                this.model.save(saveDate)
+                window.open('http://' + this.userInput, '_blank')
+                this.userInput = ''
+            } else {
+                this.inputWebsite.classList.add('animated', 'shake')
+                this.reminder.classList.add('active')
+            }
         },
         createEle: function ({ ele, klassName, parent }) {
             let element = document.createElement(ele)
-            element.className= klassName
+            element.className = klassName
             parent.appendChild(element)
             return element
         },
-        createNumberKeyboard: function ({textContent}) {
-            let key = this.createEle({ele: 'kbd',klassName: 'key',parent: this.row})
-            let number = this.createEle({ele: 'span',klassName: 'number',parent: key})
+        createNumberKeyboard: function ({ textContent }) {
+            let key = this.createEle({ ele: 'kbd', klassName: 'key', parent: this.row })
+            let number = this.createEle({ ele: 'span', klassName: 'number', parent: key })
             number.textContent = textContent
         },
         createShiftKeyboard: function () {
-            let key = this.createEle({ele: 'kbd',klassName: 'key shift',parent: this.row})
-            let icon = this.createEle({ele: 'img',klassName: 'icon',parent: key})
-            let shiftContent = this.createEle({ele: 'span',klassName: 'shiftContent',parent: key})
+            let key = this.createEle({ ele: 'kbd', klassName: 'key shift', parent: this.row })
+            let icon = this.createEle({ ele: 'img', klassName: 'icon', parent: key })
+            let shiftContent = this.createEle({ ele: 'span', klassName: 'shiftContent', parent: key })
             icon.src = './image/shift.png'
         },
-        createLetterKeyboard: function ({contentAndId}) {
-            let key = this.createEle({ele: 'kbd',klassName: 'key letterKey',parent: this.row})
+        createLetterKeyboard: function ({ contentAndId }) {
+            let key = this.createEle({ ele: 'kbd', klassName: 'key letterKey', parent: this.row })
             key.setAttribute("id", contentAndId)
-            let letter = this.createEle({ele: 'span',klassName: 'letter',parent: key})
-            let edit = this.createEle({ele: 'button',klassName: 'edit',parent: key})
-            let webIcon = this.createEle({ele: 'img',klassName: 'webIcon',parent: key})
+            let letter = this.createEle({ ele: 'span', klassName: 'letter', parent: key })
+            let edit = this.createEle({ ele: 'button', klassName: 'edit', parent: key })
+            let webIcon = this.createEle({ ele: 'img', klassName: 'webIcon', parent: key })
             this.createLetterKeyboardWebsiteIcon({
                 ele: webIcon,
                 website: this.website[contentAndId]
@@ -225,13 +230,13 @@
             letter.textContent = contentAndId
             edit.textContent = 'E'
         },
-        createLetterKeyboardWebsiteIcon: function({ele,website}){
-            if(website === ''){
+        createLetterKeyboardWebsiteIcon: function ({ ele, website }) {
+            if (website === '') {
                 ele.src = './image/smilingFace.png'
-            }else{
+            } else {
                 ele.src = 'http://' + website + '/favicon.ico'
             }
         },
     }
-    controller.init(view,model)
+    controller.init(view, model)
 }.call()
